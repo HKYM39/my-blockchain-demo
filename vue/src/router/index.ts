c404ffe559d62af5ca3e001787fd73247c6827ea
HKYM39 <hkym39@outlook.com>
2025-11-17 16:13:11 +0800

    feat: 完成简单的区块链浏览页面

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import DataView from "../views/DataView.vue";
+// import DataView from "../views/DataView.vue";
 import PortfolioView from "../views/PortfolioView.vue";
 import ChainStatusView from "../views/ChainStatusView.vue";
 
 const routerHistory = createWebHistory();
 const routes = [
   { path: "/", component: PortfolioView },
-  { path: "/data", component: DataView },
+  // { path: "/data", component: DataView },
   { path: "/chain", component: ChainStatusView },
 ];
 
--- vue/src/views/DataView.vue
+++ vue/src/views/DataView.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
 <!--     Uncomment the following component to add a form for a `modelName` -->
-     <IgntCrud store-name="OrgRepoModule" item-name="modelName" />
+<!--     <IgntCrud store-name="OrgRepoModule" item-name="modelName" />-->
   </div>
 </template>
 <script setup lang="ts">
-import IgntCrud from "@/components/IgntCrud.vue";
+// import IgntCrud from "@/components/IgntCrud.vue";
 </script>
